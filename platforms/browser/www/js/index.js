03a81d81c90b356e38dd073ba08a7d81462d98f2
Simone De Vita <info@simonedevita.it>
2019-01-14 09:35:19 +0100

    roba che si modifica da sola

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,25 +1,22 @@
-$(window).on("load",function(){
-  setTimeout(function(){
-    $("#preloader").fadeOut();
-  },500)
-})
-$(window).on('beforeunload',function(){
-  $("#preloader").fadeIn();
-})
-$(document).ready(function(){
-  $("#test_ajax").on("click", function(){
-    $.ajax({
-      type: 'POST',
-      url: 'http://fuoriserie.altervista.org/appAPI.php',
-      data: {
-        'action': "getOrders"
-      },
-      error: function(jqXHR, textStatus, errorThrown){
-        console.error("The following error occured: " + textStatus);
-      },
-      success: function(response) {
-        $("#ajax_container").html(response);
-      }
-    })
-  });
-});
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    setTimeout(function(){
+      $("#preloader").fadeOut();
+    },500)
+    $("#test_ajax").on("click", function(){
+      $.ajax({
+        type: 'POST',
+        url: 'http://fuoriserie.altervista.org/appAPI.php',
+        data: {
+          'action': "getOrders"
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+          console.error("The following error occured: " + textStatus);
+        },
+        success: function(response) {
+          $("#ajax_container").html(response);
+        }
+      })
+    });
+}
