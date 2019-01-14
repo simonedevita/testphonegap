ba0398038e5c451d6c9e33da81c5dad8c4c141ce
Simone De Vita <info@simonedevita.it>
2019-01-14 16:27:50 +0100

    device ready

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,102 +1,107 @@
-var storage = window.localStorage;
-var apiUrl = storage.getItem('apiUrl');
-var apiPsw = storage.getItem('apiPsw');
-if(apiUrl != undefined && apiUrl != null){
-  $("#api_url").val(apiUrl);
-}
-if(apiPsw != undefined && apiUrl != null){
-  $("#api_psw").val(apiPsw);
-}
+document.addEventListener("deviceready", onDeviceReady, false);
 
-function notify(notificationString, notificationCallback, notificationTitle, notificationButton){
-  if(navigator.notification != undefined && navigator.notification != 'undefined'){
-    navigator.notification.alert(notificationString, function(){}, notificationTitle, notificationButton);
-  }else{
-    window.alert(notificationString);
+function onDeviceReady() {
+  var storage = window.localStorage;
+  var apiUrl = storage.getItem('apiUrl');
+  var apiPsw = storage.getItem('apiPsw');
+  if(apiUrl != undefined && apiUrl != null){
+    $("#api_url").val(apiUrl);
   }
-}
+  if(apiPsw != undefined && apiUrl != null){
+    $("#api_psw").val(apiPsw);
+  }
 
-function getFormattedDate(date) {
-  date = new Date(date);
-  var year = date.getFullYear();
-  var month = (1 + date.getMonth()).toString();
-  month = month.length > 1 ? month : '0' + month;
-  var day = date.getDate().toString();
-  day = day.length > 1 ? day : '0' + day;
-  return day + '/' + month + '/' + year;
-}
-
-function saveSettings(){
-  var urlInput = $("#api_url").val();
-  var pswInput = $("#api_psw").val();
-  if(urlInput != 'undefined' && urlInput != null && urlInput != ''){
-    if(urlInput == 'delete'){
-      storage.removeItem('apiUrl');
-      storage.removeItem('apiPsw0');
-    }else {
-      storage.setItem('apiUrl', urlInput);
-      storage.setItem('apiPsw', pswInput);
-      apiUrl = urlInput;
-      apiPsw = pswInput;
-      getOrders();
-      changePage("#ordersPage");
+  function notify(notificationString, notificationCallback, notificationTitle, notificationButton){
+    if(navigator.notification != undefined && navigator.notification != 'undefined'){
+      navigator.notification.alert(notificationString, function(){}, notificationTitle, notificationButton);
+    }else{
+      window.alert(notificationString);
     }
-  } else{
-    notify('Url non valido', alertDismissed(), '', 'OK');
   }
-}
-//Page load
-function removeLoader(){
-  setTimeout(function(){
-    $("#preloader").fadeOut();
-  },500);
-}
-//Change page handler
-function changePage(targetPage){
-  $("#preloader").show();
-  $(".page-container").each(function(){
-    $(this).fadeOut();
-  });
-  $(targetPage).fadeIn();
-  removeLoader();
-}
-//Get Orders request
-function getOrders(){
-  if(apiUrl != undefined && apiUrl != null && apiUrl != '' && apiPsw != undefined && apiPsw != null && apiPsw != ''){
-    $.ajax({
-      type: 'POST',
-      url: apiUrl+'/appAPI.php',
-      data: {
-        'action': "getOrders",
-        'apiPsw': apiPsw
-      },
-      error: function(jqXHR, textStatus, errorThrown){
-        console.error("The following error occured: " + textStatus);
-      },
-      success: function(response) {
-        var ordersString = '';
-        console.log(response);
-        if(response != 'error'){
-          var orders = JSON.parse(response);
-          if(orders.length > 0){
-            for(var i=0;i<orders.length;i++){
-              orderDate = getFormattedDate(orders[i].date_order);
-              ordersString += '<div class="single-order">Ordine #'+orders[i].id +' del '+orderDate+'</div>';
-            }
-          }
-          $(".orders-container").html(ordersString);
-        }else{
-          $(".orders-container").html('Non è stato possibile trovare ordini, riprovare.');
-        }
 
-      }
-    });
-  }else{
-    changePage('#mainPage');
-    notify('Url non valido', null, 'Attenzione!', 'OK');
+  function getFormattedDate(date) {
+    date = new Date(date);
+    var year = date.getFullYear();
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    return day + '/' + month + '/' + year;
   }
+
+  function saveSettings(){
+    var urlInput = $("#api_url").val();
+    var pswInput = $("#api_psw").val();
+    if(urlInput != 'undefined' && urlInput != null && urlInput != ''){
+      if(urlInput == 'delete'){
+        storage.removeItem('apiUrl');
+        storage.removeItem('apiPsw0');
+      }else {
+        storage.setItem('apiUrl', urlInput);
+        storage.setItem('apiPsw', pswInput);
+        apiUrl = urlInput;
+        apiPsw = pswInput;
+        getOrders();
+        changePage("#ordersPage");
+      }
+    } else{
+      notify('Url non valido', alertDismissed(), '', 'OK');
+    }
+  }
+  //Page load
+  function removeLoader(){
+    setTimeout(function(){
+      $("#preloader").fadeOut();
+    },500);
+  }
+  //Change page handler
+  function changePage(targetPage){
+    $("#preloader").show();
+    $(".page-container").each(function(){
+      $(this).fadeOut();
+    });
+    $(targetPage).fadeIn();
+    removeLoader();
+  }
+  //Get Orders request
+  function getOrders(){
+    if(apiUrl != undefined && apiUrl != null && apiUrl != '' && apiPsw != undefined && apiPsw != null && apiPsw != ''){
+      $.ajax({
+        type: 'POST',
+        url: apiUrl+'/appAPI.php',
+        data: {
+          'action': "getOrders",
+          'apiPsw': apiPsw
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+          console.error("The following error occured: " + textStatus);
+        },
+        success: function(response) {
+          var ordersString = '';
+          console.log(response);
+          if(response != 'error'){
+            var orders = JSON.parse(response);
+            if(orders.length > 0){
+              for(var i=0;i<orders.length;i++){
+                orderDate = getFormattedDate(orders[i].date_order);
+                ordersString += '<div class="single-order">Ordine #'+orders[i].id +' del '+orderDate+'</div>';
+              }
+            }
+            $(".orders-container").html(ordersString);
+          }else{
+            $(".orders-container").html('Non è stato possibile trovare ordini, riprovare.');
+          }
+
+        }
+      });
+    }else{
+      changePage('#mainPage');
+      notify('Url non valido', null, 'Attenzione!', 'OK');
+    }
+  }
+  $("#getOrders").on("click", function(){
+    getOrders();
+  });
+  removeLoader();
+
 }
-$("#getOrders").on("click", function(){
-  getOrders();
-});
-removeLoader();
