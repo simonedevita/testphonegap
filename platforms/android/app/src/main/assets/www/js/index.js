01c9f02b027b6a5013e53eb8e6a3adb1a7a8b864
Simone De Vita <info@simonedevita.it>
2019-01-15 23:27:39 +0100

    Order detail

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -68,6 +68,10 @@ function changePage(targetPage){
   $(targetPage).fadeIn();
   removeLoader();
 }
+function showAlert(type='success',message=''){
+  $("#alert").removeClass("success").removeClass("danger");
+  $("#alert").html(message).addClass(type).fadeIn().delay(2000).fadeOut();
+}
 //Get Orders request
 function getOrders(){
   if(apiUrl != undefined && apiUrl != null && apiUrl != '' && apiPsw != undefined && apiPsw != null && apiPsw != ''){
@@ -121,20 +125,31 @@ function getOrder(orderId){
       success: function(response) {
         var orderForm = '';
         if(response != 'error'){
-          var orders = JSON.parse(response);
-          if(orders.length > 0){
-            order = orders[0];
-            orderDate = getFormattedDate(order.date_order);
-            orderForm += '<div class="order-detail"><span>Data:</span> '+orderDate+'</div>';
-            orderForm += '<div class="order-detail"><span>Totale:</span> '+order.total+'€</div>';
-            if(order.status == 'Da pagare'){
-              orderForm += '<label for="orderStatus">Stato:</label>';
-              orderForm += '<select id="orderStatus"><option selected value="Da pagare">Da pagare</option><option value="Pagato">Pagato</option></select>';
-              orderForm += '<button data-order="'+order.id+'" class="btn-default" id="saveOrder">SALVA ORDINE</button>'
-            }else{
-              orderForm = "L'ordine è stato pagato.";
+          var order = JSON.parse(response);
+          if(order.status == 'Pagato'){
+            var toPay = '';
+            var paid = 'selected';
+          }else{
+            var toPay = 'selected';
+            var paid = '';
+          }
+          orderDate = getFormattedDate(order.date_order);
+          orderForm += '<div class="order-detail"><span>Subtotale:</span> '+order.subtotal+'€</div>';
+          orderForm += '<div class="order-detail"><span>Sconto:</span> '+order.discount+'€</div>';
+          orderForm += '<div class="order-detail"><span>Totale:</span> '+order.total+'€</div>';
+          orderForm += '<div class="order-detail"><span>Data:</span> '+orderDate+'</div>';
+          orderForm += '<hr>';
+          if(order.products.length > 0){
+            orderForm += '<div class="order-detail"><span>Prodotto</span> QTA</div>';
+            for(var i=0;i<order.products.length;i++){
+              orderForm += '<div class="order-detail product-detail"><span>'+order.products[i].product.title+' - '+order.products[i].size+'</span> '+order.products[i].qta+'</div>';
             }
           }
+
+          orderForm += '<label for="orderStatus">Stato:</label>';
+          orderForm += '<select id="orderStatus">';
+            orderForm += '<option '+toPay+' value="Da pagare">Da pagare</option><option '+paid+' value="Pagato">Pagato</option></select>';
+          orderForm += '<button data-order="'+order.id+'" class="btn-default" id="saveOrder">AGGIORNA STATO</button>';
           $(".order-container").html(orderForm);
         }else{
           $(".order-container").html('Non è stato possibile trovare l\'ordine, riprovare.');
@@ -162,7 +177,7 @@ function saveOrder(orderId,orderStatus){
         console.error("The following error occured: " + textStatus);
       },
       success: function(response) {
-        notify('Ordine salvato correttamente', null, 'Attenzione!', 'OK');
+        showAlert('success', 'Ordine salvato correttamente');
       }
     });
   }else{
